a0dbd978eb8094b9acea57a89b741da862dae518
Dart_Mac <bboydart91@gmail.com>
2016-07-31 20:38:46 +0900

    git ignore and gulp file is fixed

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,6 @@ fs.readdirSync('./gulp').filter(function(file) {
  *  Default task clean temporaries directories and launch the
  *  main optimization build task
  */
-gulp.task('default', ['clean'], function () {
+gulp.task('build', ['clean'], function () {
   gulp.start('build');
 });
