9eff0ee9c661559d777604d7e62788266f5b39e3
Dart_Mac <bboydart91@gmail.com>
2016-07-31 20:01:02 +0900

    lubycon web application front is started

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('lubyconWebApp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
