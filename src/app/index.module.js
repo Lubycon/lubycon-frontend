4f0880ab40b44f7f01105964748f71a32ed24ece
Dart_DeskTop <bboydart91@gmail.com>
2016-08-02 01:24:42 +0900

    Node module and bower is removed:

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('lubyconWebApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
