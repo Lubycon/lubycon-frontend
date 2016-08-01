f79f0224c65b67ce8e670f5fb16131515d868a37
Dart_DeskTop <bboydart91@gmail.com>
2016-08-02 01:27:59 +0900

    moved files

---
+++ src/app/index.route.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+  .module('lubyconWebApp')
+  .config(routerConfig);
+
+  /** @ngInject */
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/pages/main/main.html',
+      controller: 'MainController',
+      controllerAs: 'main'
+    });
+
+    $urlRouterProvider.otherwise('/');
+  }
+
+})();
