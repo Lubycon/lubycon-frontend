ddc018f409528522bfc7eb8979cf069d98eabef1
Evan-Macbook-pro <bboydart91@gmail.com>
2016-08-22 11:18:30 +0900

    member aside is added

--- src/app/pages/member/aside.controller.js
+++ src/app/pages/member/aside.controller.js
@@ -6,14 +6,68 @@
         .controller('MemberAsideController', MemberAsideController);
 
     /** @ngInject */
-    function MemberAsideController($rootScope,$scope,API_CONFIG) {
-        var contentHost = API_CONFIG.content;
+    function MemberAsideController($rootScope, $scope, API_CONFIG) {
+
         var vm = this;
+        vm.contentHost = API_CONFIG.content;
 
-        // DUMMY DATA
+        // DUMMY DATA..나중에는 $rootScope에서 빼낼 것
         vm.member = {
-
+            code: 0,
+            name: 'Admin',
+            profile: 'user/0/profile.jpg',
+            job: 'Developer',
+            country: 'South Korea',
+            city: 'Seoul',
+            email: 'admin@lubycon.com',
+            position: 'Lubycon co.',
+            description: 'This is Test description'
         };
         // DUMMY DATA
+        vm.menu = [
+            // DASHBOARD, INSIGHT, CONTENTS, FORUMS, BOOKMARK, (MESSAGE, FOLLOWING) <- personal
+            {
+                icon: 'fa fa-tachometer',
+                name: 'Dashboard',
+                private: false,
+                link: 'aside.default.dashboard({memberId: vm.member.code})'
+            },
+            {
+                icon: 'fa fa-bar-chart',
+                name: 'Insight',
+                private: false,
+                link: 'aside.default.insight({memberId: vm.member.code})'
+            },
+            {
+                icon: 'fa fa-archive',
+                name: 'Contents',
+                private: false,
+                link: 'aside.default.memberContents({memberId: vm.member.code})'
+            },
+            {
+                icon: 'fa fa-comments-o',
+                name: 'Forums',
+                private: false,
+                link: 'aside.default.memberForums({memberId: vm.member.code})'
+            },
+            {
+                icon: 'fa fa-star',
+                name: 'Bookmark',
+                private: true,
+                link: 'aside.default.bookmark({memberId: vm.member.code})'
+            },
+            {
+                icon: 'fa fa-envelope-o',
+                name: 'Message',
+                private: true,
+                link: 'aside.default.message({memberId: vm.member.code})'
+            },
+            {
+                icon: 'fa fa-users',
+                name: 'Follow',
+                private: true,
+                link: 'aside.default.follow({memberId: vm.member.code})'
+            },
+        ];
     }
 })();
