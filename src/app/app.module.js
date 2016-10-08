dd35604b4383c89018ef2ac84768b429f99817c8
Evan-Macbook-pro <bboydart91@gmail.com>
2016-10-08 23:18:21 +0900

    timer is rollbacked

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -25,7 +25,7 @@
         'angularSpectrumColorpicker', // COLOR PICKER MODULE
         'switcher', // CHECKBOX SWITCH MODE MODULE
         'toastr', // TOAST MESSAGE MODULE
-        // 'timer', // TIMER MODULE
+        'timer', // TIMER MODULE
         'monospaced.elastic' // TEXTAREA AUTOMATICALLY SIZE
     ])
     .constant('APP_LANGUAGES', [
