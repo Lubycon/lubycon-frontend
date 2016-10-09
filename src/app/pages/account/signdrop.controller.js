8c38e732b3ca1e12559c103875792d630dbd4581
Evan-Macbook-pro <bboydart91@gmail.com>
2016-10-09 17:03:00 +0900

    sign drop is updated

--- src/app/pages/account/signdrop.controller.js
+++ src/app/pages/account/signdrop.controller.js
@@ -6,20 +6,52 @@
         .controller('SigndropController', SigndropController);
 
     /** @ngInject */
-    function SigndropController($rootScope, $scope, Restangular, Authentication) {
+    function SigndropController($rootScope, $scope, Restangular, Authentication, toastr) {
         var vm = this;
         var api = Restangular.all('members/signdrop');
 
         vm.isMobile = $rootScope.deviceInfo.isMobile;
 
         vm.dropKind = null;
-        vm.dropKindList = ['Reason A','Reason B','Reason C','etc...'];
+        vm.dropKindList = [{
+            code: '0201',
+            name: 'Reason0'
+        },{
+            code: '0202',
+            name: 'Reason1'
+        },{
+            code: '0203',
+            name: 'Reason2'
+        },{
+            code: '0204',
+            name: 'Reason3'
+        },{
+            code: '0205',
+            name: 'Reason4'
+        },{
+            code: '0206',
+            name: 'Reason5'
+        },{
+            code: '0200',
+            name: 'ETC'
+        }];
         vm.dropReason = null;
 
 
         vm.signdrop = signdrop;
         function signdrop(){
-            console.log(vm.dropKind);
+            console.log(vm.dropKind,vm.dropReason);
+            if(vm.dropKind) {
+                Restangular.all('members/signdrop').customPUT(
+                    { reasonCode: vm.dropKind, reason: vm.dropReason }
+                ).then(function(res) {
+                    console.log(res);
+                });
+            }
+            else {
+                toastr.error('회원탈퇴 이유를 선택해라 닝겐');
+            }
+
         }
     }
 })();
