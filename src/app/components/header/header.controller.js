a006c35dd6e9d49d01c773ace8f59143cc7056d9
Evan-Macbook-pro <bboydart91@gmail.com>
2016-10-22 19:44:57 +0900

    menu in header is updated

--- src/app/components/header/header.controller.js
+++ src/app/components/header/header.controller.js
@@ -14,34 +14,21 @@
         vm.isMobile = $rootScope.deviceInfo.isMobile;
         vm.isMain = $stateParams.url === '/main';
 
-        vm.menuList = {
-            content: {
-                name: 'CONTENTS',
-                link: 'common.figure.contents({ category: "3d" })'
-            },
-            community: [
-                {
-                    name: 'Creator',
-                    icon: 'fa fa-pencil',
-                    link: 'common.figure.creator({ category: null })'
-                },
-                {
-                    name: 'Forum',
-                    icon: 'fa fa-comments',
-                    link: 'common.figure.community({ category: "forum" })'
-                }
-                // {
-                //     name: 'Tutorial',
-                //     icon: 'fa fa-book',
-                //     link: 'common.figure.community({ category: "tutorial" })'
-                // },
-                // {
-                //     name: 'Q&A',
-                //     icon: 'fa fa-question',
-                //     link: 'common.figure.community({ category: "qna" })'
-                // }
-            ]
-        };
+        vm.menuList = [{
+            name: 'CONTENTS',
+            subMenu: [{
+                name: 'Explorer',
+                icon: 'fa-search',
+                link: 'common.figure.contents({ category: "3d"})'
+            }]
+        },{
+            name: 'COMMUNITY',
+            subMenu: [{
+                name: 'Forum',
+                icon: 'fa-comment',
+                link: 'common.figure.community({ category: "forum" })'
+            }]
+        }];
 
         // FOR HEADER BACKGROUND...
         $scope.scrollDetect = false;
