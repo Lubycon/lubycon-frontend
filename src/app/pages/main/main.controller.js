60fe6e1b4a7d09ba07c68bc1492f3ded6b5b1ca2
Evan-Macbook-pro <bboydart91@gmail.com>
2016-11-04 22:32:12 +0900

    mobile design is updated for main page

--- src/app/pages/main/main.controller.js
+++ src/app/pages/main/main.controller.js
@@ -19,28 +19,56 @@
                 {
                     code: 0,
                     userCode: 0,
-                    title: 'Content Title',
+                    title: 'The Battle Ship',
                     creator: 'Andrew Shanks',
                     img: 'contents/threed/Andrew_Shanks20160414050808/thumbnail/thumbnail.jpg'
                 },
                 {
                     code: 1,
                     userCode: 0,
-                    title: 'Content Title1',
+                    title: 'Marine Corps',
                     creator: 'Caroline Davies',
                     img: 'contents/threed/Caroline_Davies20160414050808/thumbnail/thumbnail.jpg'
                 },
                 {
                     code: 2,
                     userCode: 0,
-                    title: 'Content Title2',
+                    title: 'Shipping Cart',
                     creator: 'Christine Brett',
                     img: 'contents/threed/Christine_Brett20160414050808/thumbnail/thumbnail.jpg'
                 },
                 {
                     code: 3,
                     userCode: 0,
-                    title: 'Content Title3',
+                    title: 'Cuty Lady Bug',
+                    creator: 'Gwyn Collins',
+                    img: 'contents/threed/Gwyn_Collins20160414050808/thumbnail/thumbnail.jpg'
+                },
+                {
+                    code: 4,
+                    userCode: 0,
+                    title: 'Cuty Lady Bug',
+                    creator: 'Gwyn Collins',
+                    img: 'contents/threed/Gwyn_Collins20160414050808/thumbnail/thumbnail.jpg'
+                },
+                {
+                    code: 5,
+                    userCode: 0,
+                    title: 'Cuty Lady Bug',
+                    creator: 'Gwyn Collins',
+                    img: 'contents/threed/Gwyn_Collins20160414050808/thumbnail/thumbnail.jpg'
+                },
+                {
+                    code: 6,
+                    userCode: 0,
+                    title: 'Cuty Lady Bug',
+                    creator: 'Gwyn Collins',
+                    img: 'contents/threed/Gwyn_Collins20160414050808/thumbnail/thumbnail.jpg'
+                },
+                {
+                    code: 7,
+                    userCode: 0,
+                    title: 'Cuty Lady Bug',
                     creator: 'Gwyn Collins',
                     img: 'contents/threed/Gwyn_Collins20160414050808/thumbnail/thumbnail.jpg'
                 }
