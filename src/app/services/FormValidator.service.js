0fb642a3975e89277ca10fca652c29c36a3d2d1a
Dart-Ubuntu <bboydart91@gmail.com>
2016-12-09 01:29:04 +0900

    some regxs are updated to form validator

--- src/app/services/FormValidator.service.js
+++ src/app/services/FormValidator.service.js
@@ -8,18 +8,35 @@
     function FormValidateService($rootScope) {
 
         var service = {
-            nickname: nickname
+            getRegx: getRegx,
+            check: check
         };
 
         return service;
 
+        // PUBLIC METHOD
+        function getRegx(name) {
+            return eval(name);
+        }
+
+        function check(value, name) {
+            var regx = getRegx(name);
+            return regx.test(value);
+        }
+
+        // PRIVATE REGX
         function nickname() {
-            //화이트리스트가 아니라 블랙 리스트를 만들어야함
-            return {
-                minLength: 4,
-                maxLength: 20,
-                pattern: /[a-zA-Z0-9가-힣]/gi
-            };
+            // 4~20자 -,_를 제외한 특수문자 불가
+            return /[^\{\}\[\]\/?.,;:|\)*~`!^\+<>@\#$%&\\\=\(\'\"]{4,20}$/gi;
+        }
+
+        function password() {
+            // 6~255자 영문 대소문자 && 최소 1개의 숫자 혹은 특수 문자를 포함해야한다.
+            return /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{6,255}$/gi;
+        }
+
+        function ignoreSpecialWord() {
+            return /[\{\}\[\]\/?.,;:|\)*~`!-_^\+<>@\#$%&\\\=\(\'\"]/gi;
         }
     }
 })();
